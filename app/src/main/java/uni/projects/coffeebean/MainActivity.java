65ffced8b66510ba94b42e76d4b4313d957898c6
anikancheva <a.kancheva1@gmail.com>
2022-11-30 13:05:52 +0200

    order activity layout

---
+++ app/src/main/java/uni/projects/coffeebean/MainActivity.java
@@ -0,0 +1,23 @@
+package uni.projects.coffeebean;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.btnGo).setOnClickListener(view -> {
+            startActivity(new Intent(this, OrderActivity.class));
+        });
+    }
+}
